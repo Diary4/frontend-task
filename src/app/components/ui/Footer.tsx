a5162478b60a6f3d128d95cd9309be9209e4bc35
Diary Salah <diarysalah36@gmail.com>
2025-03-03 01:41:44 +0300

    feat: Creating Footer component

---
+++ src/app/components/ui/Footer.tsx
@@ -0,0 +1,150 @@
+"use client"
+
+export const Footer = () => {
+
+    const currentYear = new Date().getFullYear();
+
+    return (
+      <div className="text-black relative mt-16 bg-deep-purple-accent-400">
+        <div className="px-4 pt-12 mx-auto sm:max-w-xl md:max-w-full lg:max-w-screen-xl md:px-24 lg:px-8">
+          <div className="grid gap-16 row-gap-10 mb-8 lg:grid-cols-6">
+            <div className="md:max-w-md lg:col-span-2">
+              <a
+                href="/"
+                aria-label="Go home"
+                title="Company"
+                className="inline-flex items-center"
+              >
+                <span className="text-sky-700 ml-2 text-xl font-bold tracking-wide uppercase">
+                  mumzworld
+                </span>
+              </a>
+              <div className="mt-4 lg:max-w-sm">
+                <p className="text-sm text-deep-purple-50">
+                  Sed ut perspiciatis unde omnis iste natus error sit voluptatem
+                  accusantium doloremque laudantium, totam rem aperiam.
+                </p>
+                <p className="mt-4 text-sm text-deep-purple-50">
+                  Eaque ipsa quae ab illo inventore veritatis et quasi architecto
+                  beatae vitae dicta sunt explicabo.
+                </p>
+              </div>
+            </div>
+            <div className="grid grid-cols-2 gap-5 row-gap-8 lg:col-span-4 md:grid-cols-4">
+              <div>
+                <p className="font-semibold tracking-wide text-teal-accent-400">
+                  About Us
+                </p>
+                <ul className="mt-2 space-y-2">
+                  <li>
+                    <a
+                      href="/"
+                      className="transition-colors duration-300 text-deep-purple-50 hover:text-teal-accent-400"
+                    >
+                      Our Story
+                    </a>
+                  </li>
+                  <li>
+                    <a
+                      href="/"
+                      className="transition-colors duration-300 text-deep-purple-50 hover:text-teal-accent-400"
+                    >
+                      Our Vision
+                    </a>
+                  </li>
+                  <li>
+                    <a
+                      href="/"
+                      className="transition-colors duration-300 text-deep-purple-50 hover:text-teal-accent-400"
+                    >
+                      Our Mission
+                    </a>
+                  </li>
+                  <li>
+                    <a
+                      href="/"
+                      className="transition-colors duration-300 text-deep-purple-50 hover:text-teal-accent-400"
+                    >
+                      Our Team
+                    </a>
+                  </li>
+                </ul>
+              </div>
+              
+              <div>
+                <p className="font-semibold tracking-wide text-teal-accent-400">
+                  Contact Us
+                </p>
+                <ul className="mt-2 space-y-2">
+                  <li>
+                    <a
+                      href="/"
+                      className="transition-colors duration-300 text-deep-purple-50 hover:text-teal-accent-400"
+                    >
+                      Phone Number
+                    </a>
+                  </li>
+                  <li>
+                    <a
+                      href="/"
+                      className="transition-colors duration-300 text-deep-purple-50 hover:text-teal-accent-400"
+                    >
+                      Email Address
+                    </a>
+                  </li>
+                  <li>
+                    <a
+                      href="/"
+                      className="transition-colors duration-300 text-deep-purple-50 hover:text-teal-accent-400"
+                    >
+                      Address
+                    </a>
+                  </li>
+                  <li>
+                    <a
+                      href="/"
+                      className="transition-colors duration-300 text-deep-purple-50 hover:text-teal-accent-400"
+                    >
+                      Social Media
+                    </a>
+                  </li>
+                </ul>
+              </div>
+            </div>
+          </div>
+          <div className="flex flex-col justify-between pt-5 pb-10 border-t border-deep-purple-accent-200 sm:flex-row">
+            <p className="text-sm">
+              © Copyright all rights reserved <span className="font-bold text-sky-700">Diary</span> {currentYear}
+            </p>
+            <div className="flex items-center mt-4 space-x-4 sm:mt-0">
+              <a
+                href="/"
+                className="transition-colors duration-300 text-deep-purple-100 hover:text-teal-accent-400"
+              >
+               <svg width="24px" height="20px" viewBox="0 0 24 24" role="img" xmlns="http://www.w3.org/2000/svg">
+                <path d="M12.53.02C13.84 0 15.14.01 16.44 0c.08 1.53.63 3.09 1.75 4.17 1.12 1.11 2.7 1.62 4.24 1.79v4.03c-1.44-.05-2.89-.35-4.2-.97-.57-.26-1.1-.59-1.62-.93-.01 2.92.01 5.84-.02 8.75-.08 1.4-.54 2.79-1.35 3.94-1.31 1.92-3.58 3.17-5.91 3.21-1.43.08-2.86-.31-4.08-1.03-2.02-1.19-3.44-3.37-3.65-5.71-.02-.5-.03-1-.01-1.49.18-1.9 1.12-3.72 2.58-4.96 1.66-1.44 3.98-2.13 6.15-1.72.02 1.48-.04 2.96-.04 4.44-.99-.32-2.15-.23-3.02.37-.63.41-1.11 1.04-1.36 1.75-.21.51-.15 1.07-.14 1.61.24 1.64 1.82 3.02 3.5 2.87 1.12-.01 2.19-.66 2.77-1.61.19-.33.4-.67.41-1.06.1-1.79.06-3.57.07-5.36.01-4.03-.01-8.05.02-12.07z"/>
+               </svg>
+              </a>
+              <a
+                href="/"
+                className="transition-colors duration-300 text-deep-purple-100 hover:text-teal-accent-400"
+              >
+                <svg viewBox="0 0 30 30" fill="currentColor" className="h-6">
+                  <circle cx="15" cy="15" r="4" />
+                  <path d="M19.999,3h-10C6.14,3,3,6.141,3,10.001v10C3,23.86,6.141,27,10.001,27h10C23.86,27,27,23.859,27,19.999v-10   C27,6.14,23.859,3,19.999,3z M15,21c-3.309,0-6-2.691-6-6s2.691-6,6-6s6,2.691,6,6S18.309,21,15,21z M22,9c-0.552,0-1-0.448-1-1   c0-0.552,0.448-1,1-1s1,0.448,1,1C23,8.552,22.552,9,22,9z" />
+                </svg>
+              </a>
+              <a
+                href="/"
+                className="transition-colors duration-300 text-deep-purple-100 hover:text-teal-accent-400"
+              >
+                <svg viewBox="0 0 24 24" fill="currentColor" className="h-5">
+                  <path d="M22,0H2C0.895,0,0,0.895,0,2v20c0,1.105,0.895,2,2,2h11v-9h-3v-4h3V8.413c0-3.1,1.893-4.788,4.659-4.788 c1.325,0,2.463,0.099,2.795,0.143v3.24l-1.918,0.001c-1.504,0-1.795,0.715-1.795,1.763V11h4.44l-1,4h-3.44v9H22c1.105,0,2-0.895,2-2 V2C24,0.895,23.105,0,22,0z" />
+                </svg>
+              </a>
+            </div>
+          </div>
+        </div>
+      </div>
+    );
+  };
