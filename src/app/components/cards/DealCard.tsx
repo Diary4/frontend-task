c74a97386c48822cc372ce7ce312987a2a4e49ab
Diary Salah <diarysalah36@gmail.com>
2025-03-03 00:51:24 +0300

    style: fixing deal card styles

--- src/app/components/cards/DealCard.tsx
+++ src/app/components/cards/DealCard.tsx
@@ -10,7 +10,7 @@ interface DealCardProps {
 
 const DealCard: React.FC<DealCardProps> = ({ title, image, category, price }) => {
   return (
-    <div className="flex flex-col items-center gap-4 relative p-4 shadow-[0px_2px_3px_-1px_rgba(0,0,0,0.1),0px_1px_0px_0px_rgba(25,28,33,0.02),0px_0px_0px_1px_rgba(25,28,33,0.08)] hover:shadow-[0_3px_10px_rgb(0,0,0,0.2)] transition-all duration-300 h-[350px]">
+    <div className="flex flex-col items-center gap-4 relative p-4 my-10 mx-4 shadow-[0px_2px_3px_-1px_rgba(0,0,0,0.1),0px_1px_0px_0px_rgba(25,28,33,0.02),0px_0px_0px_1px_rgba(25,28,33,0.08)] hover:shadow-[0_3px_10px_rgb(0,0,0,0.2)] transition-all duration-300 h-[350px]">
       
       <div className="flex justify-center items-center">
         <Image src={image} alt={title} width={120} height={120} className="w-[120px] h-[120px] object-cover" />
